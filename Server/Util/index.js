712b69df9b98a47a2791d7764869893a7d561926
ompatel6355 <74576844+ompatel6355@users.noreply.github.com>
2024-06-16 14:56:27 -0400

    ICE6 files added

---
+++ Server/Util/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GenerateToken = exports.SanitizeArray = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const db_1 = __importDefault(require("../Config/db"));
+function SanitizeArray(inputString) {
+    let unsanitizedArray = inputString.split(",");
+    let sanitizedArray = Array();
+    for (const unsanitizedString of unsanitizedArray) {
+        sanitizedArray.push(unsanitizedString.trim());
+    }
+    return sanitizedArray;
+}
+exports.SanitizeArray = SanitizeArray;
+function GenerateToken(user) {
+    const payload = {
+        id: user.id,
+        DisplayName: user.displayName,
+        username: user.username,
+        EmailAddress: user.emailAddress
+    };
+    const jwtOptions = {
+        expiresIn: 604800
+    };
+    return jsonwebtoken_1.default.sign(payload, db_1.default.secret, jwtOptions);
+}
+exports.GenerateToken = GenerateToken;
+//# sourceMappingURL=index.js.map
---
+++ Server/Util/index.ts
@@ -0,0 +1,48 @@
+import jwt from 'jsonwebtoken';
+import db from '../Config/db';
+
+/**
+ * Sanitize an array of strings
+ *
+ * @export
+ * @param {string} inputString
+ * @returns {string[]}
+ */
+export function SanitizeArray(inputString: string): string[]
+{
+    let unsanitizedArray = inputString.split(",");
+    // create a new empty array container
+    let sanitizedArray = Array<string>();
+    // iterate over the unsanitizedArray and trim each string
+    for (const unsanitizedString of unsanitizedArray) 
+    {
+        sanitizedArray.push(unsanitizedString.trim());
+    }
+    return sanitizedArray;
+}
+
+/**
+ * Generate a token for the user
+ *
+ * @export
+ * @param {UserDocument} user
+ * @returns {string}
+ */
+export function GenerateToken(user: UserDocument): string
+{
+    const payload = 
+    {
+        id: user.id,
+        DisplayName: user.displayName,
+        username: user.username,
+        EmailAddress: user.emailAddress
+    }
+
+    const jwtOptions =
+    {
+        expiresIn: 604800 
+        // Note: this may be a security risk, as the token will be valid for a week
+    }
+
+    return jwt.sign(payload, db.secret, jwtOptions);
+}
